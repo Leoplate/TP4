dfd62929d2ecf88f5b89482115ea75f48ba6627d
Leonardo Cáceres <benjamonben@yahoo.com.ar>
2024-10-12 08:54:48 -0300

    Avance 12/10/24

---
+++ src/App.vue
@@ -0,0 +1,95 @@
+<script setup>
+ import Alta  from './components/Alta.vue'
+ import Baja from './components/Baja.vue'
+ import Modi from './components/Modi.vue'
+ import { ref } from 'vue';
+ const alta2 = ref(false);
+ const baja = ref(false);
+ const modi = ref(false);
+
+  const TareaAlta = () => {
+       
+       alta2.value = true;
+       baja.value = false;
+       modi.value = false;
+           
+  }
+
+  const TareaBaja = () => {
+       
+       alta2.value = false;
+       baja.value = true;
+       modi.value = false;
+           
+  } 
+
+
+  const TareaModi = () => {
+       
+       modi.value = true;
+       alta2.value = false;
+       baja.value = false;
+       
+       
+           
+  }
+
+  
+  
+  const Seguimiento = () => {
+       alert("En construccion")
+  }
+
+
+  const Salir = () => {
+    alert("En construccion")
+       
+  }
+
+  
+   
+</script>
+
+<style>
+  h1{
+    color: white;
+    background-color: black;
+    text-align: center;
+  }
+  h3{
+    color:grey
+  }
+  li{
+    color:black
+  }
+
+  #div1{
+    border-color: black;
+    border-style: solid;
+  }
+</style>
+
+<template>
+  
+  <h1>Gestión de Tareas:</h1>
+  <br><br><br>
+  <h2>Menu principal:</h2>
+  <br><br>
+  <div>
+    <ul>
+      <div id="div1">
+        <li>Tareas: </li> 
+         Alta:<input type="radio" name="opcion" @click="TareaAlta()" >   
+         Baja:<input type="radio" name="opcion" @click="TareaBaja()">
+         Modificar:<input type="radio" name="opcion" @click="TareaModi()">
+      </div>
+      <br>
+      <li>Seguimiento <button @click="Seguimiento()">-></button></li>
+      <li>Salir <button @click="Salir()">-></button></li>
+    </ul>
+  </div>
+  <br><br><br>
+  <Alta v-show="alta2"></Alta>
+  <Baja v-show="baja"></Baja>
+  <Modi v-show="modi"></Modi>
+</template>
---
+++ src/components/Alta.vue
@@ -0,0 +1,34 @@
+<template>
+    
+    
+    <form>
+        <h3>Alta:</h3>
+          Legajo:<input>
+          <br>
+          Usuario:<input>
+          <br>
+          Ingrese Tarea:<input>
+          <br>
+          <button>Crear</button>
+          <button>Salir</button>
+    
+    </form>
+    
+</template>
+
+<style scoped>
+   form {
+    color: black;
+    background-color: rgb(75, 82, 148);
+    border-style: solid;
+    border-color: blue
+   }
+   h3{
+    color: white;
+    background-color: rgb(1, 1, 26);
+   }
+</style>
+
+<script>
+   
+</script>
